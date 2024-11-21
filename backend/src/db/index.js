5d37b6e0004bbe924e762279d5ed7c2eeb2e3db7
Kunal Londhe <kunallondhex@gmail.com>
2024-11-21 21:47:47 +0530

    MONGO CONNECTED

---
+++ backend/src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async() => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log(`\n MongoDB connected!! at ${connectionInstance.connection.host} on port ${connectionInstance.connection.port}`);
+        console.log(connectionInstance.connection)
+        
+    } catch (err) {
+        console.log(`connection Failed with ${err}`);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
