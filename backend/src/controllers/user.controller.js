7375b05e4696abb116c142a753a5fd69c172e452
Kunal Londhe <kunallondhex@gmail.com>
2024-12-06 11:50:02 +0530

    MongoDB Pipeline for user Watched history .

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import User from '../models/user.model.js'
 import ApiResponse from '../utils/ApiResponse.js'
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import jwt from 'jsonwebtoken';
+import mongoose from "mongoose";
 
 
 
@@ -434,6 +435,58 @@ const getUserChannelProfile = asyncHandler( async ( req, res) => {
     .json(
         new ApiResponse(200, channel[0], "User channel fetched successfully")
     )
+});
+
+const getWatchedHistory = asyncHandler( async (req, res) => {
+    const user =  await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Schema.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullName: 1,
+                                        username: 1,
+                                        avatar: 1,
+                                    }
+                                }
+                            ]
+                        }
+                    }
+                ]
+            },
+            $addFields: {
+                owner: {
+                    $first: "$owner"
+                }
+            }
+        }
+    ])
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            user[0].watchHistory,
+            "Watched History Fetched successfully"
+        )
+    )
 })
 
 export {
@@ -447,4 +500,5 @@ export {
     updateUserAvatar,
     updateUserCoverImage,
     getUserChannelProfile,
+    getWatchedHistory
 }
